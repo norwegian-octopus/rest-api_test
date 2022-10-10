7f4284fdee2063b4f6f3ec8888507222c9743f2e
norwegian-octopus <orekhovsky.aa@gmail.com>
2022-10-10 21:06:52 +0300

    Add option to edit a data

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ async function getPosts() {
     document.querySelector('.post-list').innerHTML = '';
     posts.forEach(post => {
         document.querySelector('.post-list').innerHTML += `
-            <div class="card" style="width: 18rem">
+            <div class="card" id="post-${post.id}" style="width: 18rem">
                 <div class="card-body">
-                    <h5 class="carb-title">${post.title}</h5>
+                    <h5 class="card-title">${post.title}</h5>
                     <p class="card-text">${post.body}</p>
                     <a href="http://localhost:8012/api/posts/${post.id}" class="card-link">More</a>
-                    <a href="#" class="card-link" onclick="removePost(${post.id})">Remove post</a>
+                    <a href="#" class="card-link" onclick="removePost(${post.id})">Remove</a>
+                    <a href="#" class="card-link" onclick="editPost(${post.id}, '${post.title}', '${post.body}')">Edit</a>
                 </div>
             </div>`
     });
@@ -45,5 +46,35 @@ async function removePost(id) {
     if (data.status === true) {
         await getPosts();
     }
+}
+
+async function editPost(id, title, body) {
+
+    document.querySelector(`#post-${id}`).innerHTML = `
+            <div class="card-body">
+                <input type="text" class="form-control" id="title-edit-${id}" aria-describedby="emailHelp" value="${title}">
+                <textarea class="form-control" id="body-edit-${id}">${body}</textarea>
+                <a href="#" class="card-link" onclick="updatePost(${id})">Update</a>
+            </div>`;
+}
+
+async function updatePost(id) {
+    const title = document.querySelector(`#title-edit-${id}`).value,
+            body = document.querySelector(`#body-edit-${id}`).value;
+    
+    const data = {
+        title: title,
+        body: body
+    };
+    const res = await fetch(`http://localhost:8012/api/posts/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(data)
+    });
+
+    const resData = await res.json();
+    if (resData.status === true) {
+        await getPosts();
+    }
+
 }
 
